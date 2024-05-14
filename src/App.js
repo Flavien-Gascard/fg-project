0cdd908c05b60b831aa27e7cc8de6df0e0301f68
Flavien Gascard <flavien.gascard@gmail.com>
2024-05-14 16:19:55 -0500

    added styling, animations and gradient colors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react'
+import './App.css'
 // This is an import based off the index.js file within the Components folder
 import { Navbar, Brand, CTA } from './components';
 import { Footer, Blog, Possibility, Features, WhatFGProject, Header } from './containers';
 
 const App = () => {
-  return (
-    <div className = 'App'>
-      <div className = 'gradient_bg'>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatFGProject />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
-    </div>
-  )
+    return (
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatFGProject />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
