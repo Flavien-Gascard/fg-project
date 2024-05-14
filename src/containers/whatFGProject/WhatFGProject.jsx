16599d85e73ddf55dcdd150840bdac00999b08b9
Flavien Gascard <flavien.gascard@gmail.com>
2024-05-14 15:34:55 -0500

    Created folder structures - Added blank shell components - Created component and container export index files

---
+++ src/containers/whatFGProject/WhatFGProject.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WhatFGProject.css'
+
+const WhatFGProject = () => {
+  return (
+    <div>
+      What Is The F.G. Project
+    </div>
+  )
+}
+
+export default WhatFGProject
