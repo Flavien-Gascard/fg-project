b7f7028f224b9a58892beacae01ffb39e6fb4aec
Flavien Gascard <flavien.gascard@gmail.com>
2024-05-16 17:14:09 -0500

    navbar styling - end of day commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './Navbar.css';
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    // Notice the className? It is in BEM (Block Element Modifier) format, a common css convention
+    <div className="fgproject__navbar">
+      <div className="fgproject__navbar-links">
+        <div className="fgproject__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="fgproject__navbar-links_container">
+        <p><a href="#home" />Home</p>
+        <p><a href="#whatFGProject" />About F.G Project</p>
+        <p><a href="#possibility" />Interests</p>
+        <p><a href="#features" />Case Studies</p>
+        <p><a href="#blog" />Library</p>
+      </div>
     </div>
+    <div className ="fgproject__navbar-sign">
+<p>Sign In</p>
+    <button type='button'>Sign Up</button>
+
+    </div>
+    </div>
+
   )
 }
 
