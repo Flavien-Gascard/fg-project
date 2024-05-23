3107566afc86e224591da4440b129b8a095ca508
Flavien Gascard <flavien.gascard@gmail.com>
2024-05-22 19:42:10 -0500

    Finished with Nav bar

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Brand.css';
+import { reactlogo, awslogo, javalogo, dockerlogo, githublogo } from './imports';
 
 const Brand = () => {
   return (
-    <div>
-      <h1>Brand</h1>
+    <div className="fgproject__brand section__padding">
+      <div><img src={ reactlogo } alt="React" /></div>
+      <div><img src={ awslogo } alt="AWS" /></div>
+      <div><img src={ javalogo } alt="Java" /></div>
+      <div><img src={ dockerlogo } alt="Docker" /></div>
+      <div><img src={ githublogo } alt="Github" /></div>
+
     </div>
   )
 }
+
 
 export default Brand
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import reactlogo from '../../assets/react-logo.png';
+import javalogo from '../../assets/java-logo.png';
+import awslogo from '../../assets/aws-logo.png';
+import dockerlogo from '../../assets/docker-logo.png';
+import githublogo from '../../assets/github-logo.png';
+
+export {
+    reactlogo,
+    javalogo,
+    awslogo,
+    dockerlogo,
+    githublogo
+}
