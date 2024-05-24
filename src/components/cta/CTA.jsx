7dba3c858092b64d6255ef5de932aa442cca994e
Flavien Gascard <flavien.gascard@gmail.com>
2024-05-24 05:46:02 -0500

    finished the possibility section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,16 @@ import './CTA.css';
 
 const CTA = () => {
   return (
-    <div>
-      <h1>CTA</h1>
+    <div className="fgproject__cta">
+      <div className="fgproject__cta-content">
+
+<p>Request early access to get started</p>
+<h3>Register today and let's start learning full stack development!</h3>
+
+      </div>
+<div className="fgproject__cta-btn">
+  <button type = "button">Start here</button>
+</div>
     </div>
   )
 }
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Possibility.css'
+import possibilityImage from "../../assets/computer-logo.jpg"
 
 const Possibility = () => {
   return (
-    <div>
-      POSSIBILITY
+    <div className="fgproject__possibility section__padding" id="possibility">
+      <div className="fgproject__possibility-image">
+
+        <img src={possibilityImage} alt="Possibility"/>
+      </div>
+
+      <div className="fgproject__possibility-content">
+        <h4>Get Started</h4>
+        <h1 className="gradient__text">You are not alone. I have had many experiences I would like to share!</h1>
+        <p>Through trial and error I have been looking for work and found it very daunting. I want to document my experiences so I may share them with you!</p>
+      
+      <h4>Request help and let's get started!</h4>
+      
+      </div>
     </div>
   )
 }
