7d5f10576743ac2e2261eca7241885a2d8615451
Flavien Gascard <flavien.gascard@gmail.com>
2024-05-29 16:46:45 -0500

    Added Navbar and Header components and containers

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,28 @@
+import logo from '../assets/logo.png';
+import bgWhite from '../assets/bgWhite.png';
+import profile from'../assets/profile.png';
+import circle from '../assets/circle.svg';
+import awslogo from '../assets/aws-logo.png';
+import githublogo from'../assets/github-logo.png';
+
+import javalogo from'../assets/java-logo.png';
+import javascriptlogo from'../assets/javascript-logo.png';
+import reactlogo from '../assets/react-logo.png';
+import kuberneteslogo from '../assets/kubernetes-logo.png';
+import dockerlogo from '../assets/docker-logo.png';
+import bgIMG from '../assets/bgIMG.png'
+
+export default {
+    logo,
+    bgWhite,
+    profile,
+    circle,
+    awslogo,
+    githublogo,
+    javalogo,
+    javascriptlogo,
+    reactlogo,
+    kuberneteslogo,
+dockerlogo,
+bgIMG
+}
