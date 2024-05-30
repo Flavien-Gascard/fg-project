cb3bec12358b43849c23c18e9c903961b506714d
Flavien Gascard <flavien.gascard@gmail.com>
2024-05-30 17:29:54 -0500

    Added skills component along with sanity tables

--- backend_sanity/schemaTypes/index.js
+++ backend_sanity/schemaTypes/index.js
@@ -1,13 +1,18 @@
 import testimonials from "./testimonials"
-
+import workExperience from "./workExperience"
 import abouts from "./abouts"
 import works from "./works"
-
+import experiences from './experiences'
+import skills from "./skills"
 
 
 export const schemaTypes = [
 
-testimonials,abouts,works
-
+testimonials,
+abouts,
+works,
+experiences, 
+workExperience,
+skills
 
 ]
---
+++ backend_sanity/schemaTypes/skills.ts
@@ -0,0 +1,31 @@
+import { defineField, defineType } from 'sanity';
+
+
+export const skills = defineType({
+    name:'skills',
+    title:'Skills',
+    type: 'document',
+    fields:[
+        {
+            name:'name',
+            title:'Name',
+            type:'string'
+        },
+        {
+            name:'bgColor',
+            title:'BgColor',
+            type:'string'
+        },
+        {
+            name:'icon',
+            title:'Icon',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },
+        
+    ]
+})
+
+export default skills;
