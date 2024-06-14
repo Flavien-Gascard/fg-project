3fc83f32d75259181abe6a442183929ffca86506
Flavien Gascard <flavien.gascard@gmail.com>
2024-06-13 23:06:49 -0500

    Header is giving me a hard time - re-factoring.....

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -5,16 +5,16 @@ import { images } from '../../constants'
 import {AppWrap} from '../../wrapper'
 
 
-const scaleVariants = {
-  whileInView: {
-    scale: [0, 1],
-    opacity: [0, 1],
-    transition: {
-      duration: 1,
-      ease: 'easeInOut',
-    },
-  },
-};
+// const scaleVariants = {
+//   whileInView: {
+//     scale: [0, 1],
+//     opacity: [0, 1],
+//     transition: {
+//       duration: 1,
+//       ease: 'easeInOut',
+//     },
+//   },
+// };
 
 const Header = () => (
   <div className="app__header app__flex">
@@ -23,55 +23,32 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" />
+    
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.profile}
+        alt="header_profile_image"
+        className="header_image"
+      />
+
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
         src={images.headerCircle}
-        alt="profile_circle"
-        className="overlay_circle"
+        alt="header_profile_circle"
+        className="header_circle"
       />
     </motion.div>
 
     <motion.div
-      variants={scaleVariants}
-      whileInView={scaleVariants.whileInView}
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-circles"
-      transition={{ duration: 1, ease: 'easeInOut' }}
     >
-      {/* {[images.javascriptlogo, images.reactlogo, images.awslogo].map((circle, index) => ( */}
-        <motion.div 
-        className="circle-cmp app__flex" 
-        // key={`circle-${index}`}
-        whileTap={{ scale: 3, rotate: 360, zIndex:1}}
-        transition={{ duration: 0.5 }}
-        // whileTap={{ scale: 0.8 }}    // Scale down to 0.8 on tap
-        >
-          <img src={images.javalogo} alt="profile_bg" />
-        </motion.div>
-      {/* ))} */}
-      <motion.div 
-        className="circle-cmp app__flex" 
-        // key={`circle-${index}`}
-        whileTap={{ scale: 3, rotate: 360, zIndex:1}}
-        transition={{ duration: 0.5 }}
-        >
-          <img src={images.reactlogo} alt="profile_bg" />
-        </motion.div>
+Place holder
+      </motion.div>
 
-        <motion.div 
-        className="circle-cmp app__flex" 
-        // key={`circle-${index}`}
-        whileTap={{ scale: 3, rotate: 360, zIndex:1}}
-        transition={{ duration: 0.5 }}
-        >
-          <img src={images.awslogo} alt="profile_bg" />
-        </motion.div>
-
-
-
-
-    </motion.div>
   </div>
 );
 export default AppWrap(Header, 'home')
