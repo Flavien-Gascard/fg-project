e478d2eedc147b616b5e642fa11c4fe3bb2d8f14
Flavien Gascard <flavien.gascard@gmail.com>
2024-06-17 13:22:55 -0500

    More header adjustments

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -6,50 +6,39 @@ import { AppWrap } from '../../wrapper'
 
 
 const Header = () => (
-  <div className="app__header app__flex">
-    <motion.div
-      whileInView={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5, delayChildren: 0.5 }}
-      className="app__header-img"
-    >
+  <div className="header">
 
-<motion.img
+    <div className='profile'>
+      <motion.img
         whileInView={{ scale: [0, 1] }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
+        animate={{ rotate: [0, 360, -360, 0] }}
+        transition={{
+          duration: 1,
+          ease: 'linear',
+        }}
         src={images.headerCircle}
-        alt="header_profile_circle"
-        className="header__circle"
+        alt="profile_circle"
+        className="profile_circle"
       />
       <motion.img
         whileInView={{ scale: [0, 1] }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
+        transition={{ duration: 1.5, ease: 'easeInOut' }}
         src={images.profile}
-        alt="header_profile_image"
-        className="header__profile"
+        alt="profile_image"
+        className="profile_image"
       />
-    </motion.div>
+    </div>
 
-    <motion.div
-      whileInView={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5, delayChildren: 0.5 }}
-      className="app__header-thoughts"
-    >
-      <motion.img
-        src={images.reactlogo}
-        className='thinking__java'
-      />
+    <div className='logos'>
 
-      <motion.img
-        src={images.awslogo}
-        className='thinking__aws'
-      />
+      <img src={images.reactlogo} alt="React Logo" />
 
-      <motion.img
-        src={images.javalogo}
-        className='thinking__java'
-      />
+      <img src={images.awslogo} alt="AWS Logo" />
 
-    </motion.div>
+      <img src={images.javalogo} alt="Java Logo" />
+
+
+    </div>
 
   </div>
 );
